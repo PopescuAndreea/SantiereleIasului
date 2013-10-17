b11490eca93eb431772699b6de95ab1adde97b96
PopescuAndreea <andreea@Andreeas-MacBook-Pro.local>
2013-10-17 20:43:42 +0300

    First commit

---
+++ js/iasi.js
@@ -0,0 +1,126 @@
+(function() {
+  "use strict";
+
+  var locations = [
+    {
+      title: "Pasajul Octav Băncilă",
+      details: "Pasajul Octav Băncilă este inclus în proiectul Axa de Dezvoltare Nord-Sud, iar perioada de implementare a acestuia este 16.09.2010 – 16.09.2013. Pasajul va avea o lungime de 532.2 metri și o lățime de 7.8 metri. Prin intermediul pasajului se va rezolva o problemă mai veche, cea a interconectării a două zone mari din Iași, Alexandru-Dacia și Canta-Păcurari prin realizarea unui pasaj suprateran de transport peste calea ferată.<br>Lucrările, unde este posibil, vor continua și pe timp de iarnă, urmând ca din primăvară să se reia construcția pasajului și a zonelor aferente integral. Zonele pentru reabilitare incluse în Axa de Dezvoltare Nord-Sud unde nu se poate lucra pe timp de iarnă au intrat în conservare și lucrările vor începe în primăvară.<br>Acest proiect ambitios are menirea de a realiza legătura dintre cartierele Dacia și Alexandru cu centrul orașului. De asemenea, în această zonă s-au realizat lucrări de deviere a rețelei de apă și canalizare, lucrări ce au fost  oprite pe timp de iarnă și care produc nemulțumiri locuitorilor din zonă, deoarece le este afectată fundația caselor. Tot în cadrul proiectului se vor moderniza și străzile adiacente, strada Sarmisegetuza, Tabacului, Columnei, Vitejilor, Mușatini, Petru Poni și Toma Cozma.<br>Lupășteanu Amalia Paula",
+      images: [
+        "images/s1/1.jpg",
+        "images/s1/2.jpg",
+        "images/s1/3.jpg"
+      ],
+      x: 268,
+      y: 147
+    },
+    {
+      title: "Consolidarea Palatului Culturii",
+      details: "Șantierul Palatului Culturii din Iași și-a înălțat pentru dată schelele în 2008, deși a fost închis pentru studii de fezabilitate în vederea efectuării lucrărilor de renovare și înainte. Ceea ce s-a presupus a fi o operațiune complexă de restaurare s-a complicat și mai mult în momentul în care, în timpul renovării, s-au găsit vestigii arheologice în subsolul clădirii, beciuri și sisteme de canalizare care au necesitat amplasarea unui sit arhelogic și de investigații suplimentare. La nivel de fonduri, prima sumă estimată a fi necesară a fost de 7 milioane de euro, aceasta fiind suplimentată imediat cu 3,5, în contextul descoperirii vestigiilor respective.<br>La sfîrșitul anului trecut, după o perioadă în care s-au efectuat doar lucrări de amenajare exterioară, Ministerul Culturii, cu aprobarea Guvernului, a alocat o nouă sumă de 16 milioane de euro pentru a duce la bun sfîrși lucrările de renovare. Dacă primii bani au venit și prin sprijinul Băncii de Dezvoltare  a Consiliului Europei, toată suma de 16 milioane de euro va fi suportat de către statul român deoarece, în acordul stabilit cu BDCE, se precizează că orice alte cheltuieli care se îndepărtează de planul inițial trebuie să fie suportate de stat.<br>În legătură cu banii primiți din fonduri nerambursabile au existat discuții și acuzații aduse fostei conduceri a Complexului Muzeal Național „Moldova”, care gestionează Palatul Culturii, că ar fi folosit impropriu finanțarea și că au tergiversat desfășurarea lucrărilor. Lucrări care, conform unui interviu oferit de dirigentele de șantier, Tiberiu Makkai, în acest moment sînt de natură de recondiționare, estetică, consolidarea fiind aproape terminată. Însă acestea au durat o perioadă mai lungă decît a fost prevăzut inițial și pentru că studiul de fezabilitate nu ar fi estimat corectat nivelul de degradare al zidurilor după ce au fost „dezbrăcate” de elementele estetice exterioare.<br>Pînă în prezent acesta estimează că proiectul ar fi terminat în proporție de 60%. Ultimele estimări ale datei limită de terminare a lucrărilor precizează că acestea vor fi gata cel mai devreme în doi ani de zile, în 2015. Astfel, lucrările vor fi desfășurate, din 2008, de la inițiere, pe o durata de cel puțin 7 ani. Palatul Culturii din Iași este a doua clădire ca dimensiune din România, cu o suprafață de 28.000 de metri pătrați, după Casa Poporului.<br>Hopulele Cătălin",
+      images: [
+        "images/s2/1.jpg",
+        "images/s2/2.jpg",
+        "images/s2/3.jpg"
+      ],
+      x: 488,
+      y: 300
+    },
+    {
+      title: "Pasajul Mihai Eminescu",
+      details: "Pasajul Mihai Eminescu face parte din proiectul de dezvoltarea Axei de Transport Est-Vest în Iași. Pasajul subteran are o lungime de 286,4 m, o înălțime de 5 metri și o lățime de 10 metri și va avea rampe de acces dinspre strada Păcurari și Bulevardul Independeței. Perioada de implementare a acestuia este 17.09.2010 – 16.07.2013.<br>Proiectul își propune construirea unui pasaj subteran in Piața Mihai Eminescu, reabilitarea spațiilor pietonale aferente străzilor reabilitate (trotuare), crearea de piste de bicicliști, îmbunătățirea sistemului de marcaj rutier, reabilitarea sistemului de preluare a apelor pluviale și reabilitarea liniei de tramvai de pe strada Elena Doamna. Pasajul Mihai Eminescu se află într-un stadiu de finalizare de 40%, rețelele de utilități au fost deviate în proporție de 100%.<br>De la jumătatea lunii ianuarie se redeschide șantierul unde se poate lucra și pe timp friguros. Săpăturile din această zonă nu vor afecta liniile de tramvai care, deocamdată, vor rămâne pe aceeași poziție. Lucrările se află în graficul stabilit. Însă, creează numai inconveniențe traficului din Iași, deoarece lucrările au blocat și îngreunat traficul din centrul orașului, uneori liniile de tramvai fiind închise.<br>Lupășteanu Amalia Paula",
+      images: [
+        "images/s3/1.jpg",
+        "images/s3/2.jpg",
+        "images/s3/3.jpg"
+      ],
+      x: 385,
+      y: 157
+    },
+    {
+      title: "Bulervardul Stefan cel Mare - pietonal",
+      details: "Lucrările la șantierul de pe Bulevardul Ștefan cel Mare și Sfînt au presupus de la bun început realizarea unei alei pietonale din dreptul Teatrului Național pînă la întîlnirea cu drumul care trece prin fața Palatului Culturii. O piață comună între Mitropolie și Primărie, și-au declarat oficialitățile ieșene intenția. Problema s-a ivit atunci cînd a venit vorba de executarea lucrărilor. Inițial acestea trebuiau să oprească de tot traficul de mașini care curgea înspre Piața Unirii pe Bulevardul Ștefan cel Mare și Sfînt însă, în final, s-a ajuns la o soluție de compromis care a presupus realizarea pasajului pietonal dinspre ieșirea la strada din fața Palatului Culturii și pînă la strada I. C. Brătianu, care virează stînga imediat înainte de Mitropolie, spre Teatrul Național.<br>Implementarea proiectul a început în cursul lunii iunie și a avut mai multe termene de finalizare. Primul dintre acestea marca începerea sărbătorii zilelor orașului, din luna octombrie, însă lucrările nu au fost finalizate la timp. Mai mult, la începutul lunii septembrie, o suprafață de aproximativ 60 de metri pătrați de piatră cubică care fusese deja amplasată a trebuit să fie scoasă. Oficialitățile au pretins că acest lucru s-a datorat faptului că piatra era pusă de probă însă după ce a fost scoasă, pe tronsonul respectiv a fost instalată o nouă serie de conducte îngropate.<br>Al doilea termen de finalizare era dat ca fiind sfîrșitul anului 2012, însă nici pînă atunci piatra nu a fost montată integral, muncitorii lucrînd pînă în ultimele zile ale lui decembrie. În prezent, deși montajul pietrei este terminat, lipsesc încă lucrările efectuate de modernizare a rețelei electrice și de montare a unor stîlpi de iluminat industriali cu unii decorativi.<br>Lucrările de renovare sînt realizate din fonduri europene ne-rambursabile, care sînt destinate modernizării zonei cultural-istorice a Iașului. Din același proiect mai fac parte și lucrări de modernizare și de asfaltare a 16 străzi din centrul orașului, de remediere a aproape 4 kilometri de linie de tramvai, cît și amenjarea pistelor de biciliști în zona centrală. Întreg proiectul ajunge la o valoare de 91 de milioane de lei, ceva mai mult de 20 de milioane de euro.<br>Hopulele Cătălin",
+      images: [
+        "images/s4/1.jpg",
+        "images/s4/2.jpg",
+        "images/s4/3.jpg"
+      ],
+      x: 455,
+      y: 240
+    },
+    {
+      title: "Înlocuirea hidroizolației la esplanada de la Hala Centrală și refacerea pavajului",
+      details: "Începute în data de 20 august 2012, lucrările de pe esplanada Halei Centrale din Iași continuă și în momentul de față. Proiectul include refacerea pavajului, precum și înlocuirea hidroizolației de pe esplanadă. Între timp, s-a decis și plantarea unor arbori de mici dimensiuni pe platoul din fața Halei, dar și montarea unui zid de piatră pe care va fi indicat traseul către pasaj.<br>Lucrările sunt realizate de către firma CONEST S.A., din a cărui portofoliu mai fac parte și alte lucrări de mare anvergură din Iași, precum pasajul Octav Băncilă, căminul Akademos al Universității Alexandru Ioan Cuza sau reabilitarea Filarmonicii de Stat Moldova.<br>Durata acestor lucrări a fost estimată la 4 luni de zile, însă acesta s-a prelungit din cauza sosirii iernii. Costurile totale ale acestor lucrări, în care sunt prinse și refacerea scărilor și acceselor către piață, sunt de 1,6 milioane lei, bani ce fac parte din proiectul pasajului Hala Centrală.<br>Municipalitatea ieșeană a decis inclusiv schimbarea denumirii Halei Centrale în Piața Sfânta Vineri, după numele vechii ulițe care a existat în acel loc în secolul XVII. Pasajul Sfânta Vineri a fost inaugurat la începutul lunii iunie 2012, deși nu era complet finalizat, chiar înaintea alegerilor electorale, în prezența oficialităților locale, după 6 ani de lucrări. Costurile totale au ajuns la aproximativ 22 de milioane de lei. Se poate ajunge la pasaj prin trei puncte de acces, unul la esplanada Halei Centrale, unul la Casa de Pensii şi unul spre strada Sfântu Lazăr. Pasajul se întinde pe o suprafaţă de 1.830 mp, peste demisolul și pivnițele unei vechi case boierești din secolul XVII și conține 491 mp de vestigii și 448 mp de spații comerciale.<br>Mihăeș Alina",
+      images: [
+        "images/s5/1.jpg",
+        "images/s5/2.jpg",
+        "images/s5/3.jpg"
+      ],
+      x: 510,
+      y: 238
+    },
+    {
+      title: "Regularizarea râului Bahlui",
+      details: "În prezent, în județul Iași se desfășoară un proiect apolitic pe rîul Bahlui, în care, de la bun început, se identifică 3 piloni majori: infrastructura de mediu, infrastructura de turism și agreement și implicarea partenerilor privați și a societății civile, pentru revitalizarea economico-socială a zonei centrale a Iașului.<br>La nivelul infrastructurii de mediu, în municipiul Iași se lucrează la regularizarea râului Bahlui. Lucrările au început în luna martie a anului 2011 și urmează a fi terminate în vara naului 2013. Obiectivul de investiție Regularizare râu Bahlui în Municipiul Iași prevede realizarea în municipiul Iași a unor lucrări de asigurare a continuității pantei albiei râului Bahlui și stabilizarea malurilor (incluzând decolmatarea albiei râului Bahlui pe o lungime de 11 km, lucrări pentru stabilizarea malurilor albiei minore în zona în care se realizează decolmatarea, reprofilarea albiei minore pe o lungime  totală de 6,13 km, îmbrăcăminți cu pereți din beton armat în albia minoră pe o lungime de 2,5 km între Podul de Piatră și Podul Tudor Vladimirescu, stabilizarea albiei minore cu gabioane din „amonte de confluență cu râul Nicolina” până la Podul de Piatră și între Podul Tudor Vladimirescu și Podul Metalurgiei), precum și lucrări pentru asigurarea liniei continue de apărare împotriva inundațiilor a orașului Iași (realizarea unui dig de apărare pe malul drept -3,83 km lungime în zona Cicoarei, refacerea parapeților de apărare pe o lungime de 2,5 km între Podul de Piatră și Podul Tudor Vladimirescu, supraînălțarea digului existent pe o lungime de 2.97 km între Podul Tudor Vladimirescu și Podul Sfântul Ioan.<br>În ceea ce privește infrastructura de turism și agreement, pe tronsonul central al râului Bahlui, între Podul de Piatră și Podul Tudor Vladimirescu, pe cei 2,5 km ai sectorului central din municipiul Iași vor fi amenajate: alei pietonale și pentru bicicliști, bănci, locuri de odihnă și recreere, iluminat public ambiental, mici terase și spații comerciale, acumulare nepermanentă (cu stăvilar) care să permită navigația cu ambarcațiuni ușoare, sportive și de agreement.<br>Mihăeș Alina",
+      images: [
+      ],
+      x: 438,
+      y: 341
+    }
+  ];
+
+  var map = document.getElementById("map");
+  var bubble = document.getElementById("bubble");
+  var bubbleTitle = document.getElementById("bubble-title");
+  var bubbleDetails = document.getElementById("bubble-details");
+  var bubbleAnchors = document.getElementsByClassName("bubble-image-anchor");
+  var bubbleImages = document.getElementsByClassName("bubble-image");
+
+  locations.forEach(function(location){
+    var img = document.createElement("img");
+    img.className = "pin";
+    img.src = "images/pin.png";
+    img.style.left = location.x + "px";
+    img.style.top = location.y + "px";
+
+    img.addEventListener("mouseover", function(e) {
+      bubble.classList.add("hidden");
+
+      window.setTimeout(function() {
+        bubble.classList.remove("hidden");
+        bubble.style.left = location.x + "px";
+        bubble.style.top = location.y + "px";
+        bubbleTitle.textContent = location.title;
+        bubbleDetails.innerHTML = location.details;
+
+        for (var i = 0; i < 4; i++) {
+          var src = location.images[i];
+          var anchor = bubbleAnchors[i];
+          var image = bubbleImages[i];
+          if (src) {
+            anchor.href = image.src = src;
+            anchor.title = location.title;
+            image.classList.remove("hidden");
+          } else {
+            image.classList.add("hidden");
+          }
+        }
+      }, 200);
+    });
+
+    map.appendChild(img);
+  });
+
+  map.addEventListener("click", function() {
+    bubble.classList.add("hidden");
+  });
+  document.body.addEventListener("click", function() {
+    bubble.classList.add("hidden");
+  });
+
+  map.addEventListener("mousemove", function(e) {
+    var mapBounds = map.getBoundingClientRect();
+    var mouseX = e.clientX - mapBounds.left;
+    var mouseY = e.clientY - mapBounds.top;
+    console.log([mouseX, mouseY]);
+  });
+
+})();
